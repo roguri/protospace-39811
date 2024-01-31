9286858aa9167aef7424ff911814469a94b59a64
roguri <ryoryoma205@gmail.com>
2024-01-31 16:40:10 +0900

    Issue修正2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   def show
-    @user = User.find(params[:id])
-    @prototypes = @user.prototypes
+    @user = User.find_by(id: params[:id])
+    @prototypes = Prototype.where(user_id: params[:id])
   end
 end
